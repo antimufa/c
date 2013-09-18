1841bf6628fdd0aab17f4529e260d330f7265e12
Cris245 <cristians245@gmail.com>
2013-09-18 19:10:11 -0300

    Tp2
    Se creo la estructura basica

---
+++ Tp2Archivos/tp2Archivos.c
@@ -0,0 +1,20 @@
+#include tp2archivos.h
+void verArchivo(FILE * pf)
+{
+    int i;
+    datos d;
+    pf= fopen("arch.bin","rb");
+   if(!pf)
+    {
+        printf("Error al abrir el archivo");
+        exit(1);
+    }
+    fread(&d,sizeof(datos),1,pf);
+    while (!feof(pf))
+    {
+        printf("\n ESCRITURA %s\n NUMERO %d\t FLOAT: %f\n",d.letras,d.entero,d.flotante);
+        fread(&d,sizeof(datos),1,pf);
+        i++;
+    }
+fclose(pf);
+}
---
+++ Tp2Archivos/tps2archivos.h
@@ -0,0 +1,12 @@
+#ifndef TPS2ARCHIVOS_H_INCLUDED
+#define TPS2ARCHIVOS_H_INCLUDED
+void verArchivo(FILE *);
+typedef struct {
+    int legajo;
+    char estado;
+    char nombre[50];
+    float algo;
+
+}
+
+#endif // TPS2ARCHIVOS_H_INCLUDED
